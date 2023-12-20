32aaab9d869da16bb1c61e7c9494378aeed749f2
Леонид Иванов <ivanov.l@robotkarl.com>
2023-12-20 20:12:46 +0800

    add port in bd

--- src/store/rooms.js
+++ src/store/rooms.js
@@ -15,6 +15,10 @@ export const useRoomsStore = defineStore('rooms', () => {
   const roomUserStore = useRoomUserStore()
 
   const createConnection = async () => {
+    console.log(window.location)
+    console.log(window.location.host)
+    console.log(window.location.href)
+
     socket.value = await io('/');
 
      axios.get('/users')
