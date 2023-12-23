c5d8e6d5bd780087a59e0159aa12f31692fdffde
Леонид Иванов <ivanov.l@robotkarl.com>
2023-12-23 18:03:05 +0800

    check vercel view

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue/',
+  // base: '/vue/',
   plugins: [vue()],
-  server: {
-    port: 3000
-  },
+  // server: {
+  //   port: 3000
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Укажите путь к вашей папке src
